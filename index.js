f1a411abdee246178a111f0fdb4b08a85560c197
Halil güler <h.guler3717@gmail.com>
2019-08-31 14:17:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(item) {
+  drivers.push(item);
+}
+
+function destructivelyPrependDriver(item) {
+  drivers.unshift(item);
+}
+
+function destructivelyRemoveLastDriver(item) {
+  drivers.pop(item);
+}
+
+function destructivelyRemoveFirstDriver(item) {
+  drivers.shift();
+}
+
+function appendDriver(item) {
+  return [...drivers, name];
+}
+
+function prependDriver(item) {
+  return [name, ...drivers];
+}
+
+function removeFirstDriver() {
+  return drivers.slice(1);
+}
+
+function removeLastDriver(){
+  return drivers.dlice(0, drivers.length-1);
+}
+
